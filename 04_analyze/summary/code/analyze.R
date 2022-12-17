2a0db2274d756c040cec007a7ebdbb28acff6049
h1ra0ka <hiraoka-rintaro-zb@ynu.jp>
2022-12-17 14:38:12 +0000

    made summary

--- 04_analyze/summary/code/analyze.R
+++ 04_analyze/summary/code/analyze.R
@@ -1,7 +1,7 @@
 main <- function(){
   read_master()
   summarize(master)
-  make_table(JPN_summary, USA_summary)
+  make_table(both_summary, JPN_summary, USA_summary)
 }
 
 read_master <- function(){
@@ -10,13 +10,28 @@ read_master <- function(){
 }
 
 summarize <- function(master){
+  both_summary <- dplyr::select(master, gini) %>%
+    summary() %>%
+    as.vector()
   JPN_summary <- dplyr::filter(master, country == "JPN") %>%
-    dplyr::select(-country, -year) %>%
+    dplyr::select(gini) %>%
       summary()
-  
-  return(JPN_summary, USA_summary)
+  USA_summary <- dplyr::filter(master, country == "USA") %>%
+    dplyr::select(gini) %>%
+    summary()
+    
+  return(both_summary, JPN_summary, USA_summary)
 }
 
-make_table <- function(JPN_summary, USA_summary){
-  kableExtra::kbl(JPN_summary)
+make_table <- function(both_summary, JPN_summary, USA_summary){
+  both_numbers <- strsplit(both_summary, ":")
+  both_min <- both_numbers[[1]]
+  both_1Q <- both_numbers[[2]]
+  both_median <- both_numbers[[3]]
+  both_mean <- both_numbers[[4]]
+  both_3Q <- both_numbers[[5]]
+  both_max <- both_numbers[[6]]
+  
+  both_numbers2 <- c(both_min[[2]],both_1Q[[2]], both_median[[2]], both_mean[[2]], both_3Q[[2]], both_max[[2]])
+  
 }
